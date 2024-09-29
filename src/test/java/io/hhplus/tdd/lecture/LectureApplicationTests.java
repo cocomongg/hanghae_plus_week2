a1e8bc29ceb0603bc094bcc40fced9828f422092
cocomongg <pjm4142@naver.com>
2024-09-29 22:58:10 +0900

    initial commit

---
+++ src/test/java/io/hhplus/tdd/lecture/LectureApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.tdd.lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
