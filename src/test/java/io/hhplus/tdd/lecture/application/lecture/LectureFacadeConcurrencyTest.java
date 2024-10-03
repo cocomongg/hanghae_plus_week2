f43fddfd3cce7b52ef78ac29165418261def2c39
cocomongg <pjm4142@naver.com>
2024-10-04 03:19:57 +0900

    test: 동일한 유저가 특강을 5번 신청했을 때, 1번만 성공하는 검증 테스트

--- src/test/java/io/hhplus/tdd/lecture/application/lecture/LectureFacadeConcurrencyTest.java
+++ src/test/java/io/hhplus/tdd/lecture/application/lecture/LectureFacadeConcurrencyTest.java
@@ -2,7 +2,9 @@ package io.hhplus.tdd.lecture.application.lecture;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import io.hhplus.tdd.lecture.domain.lecture.exception.LectureException;
 import io.hhplus.tdd.lecture.infrastructure.db.entity.lecture.Lecture;
+import io.hhplus.tdd.lecture.infrastructure.db.entity.lecture.LectureApplyHistory;
 import io.hhplus.tdd.lecture.infrastructure.db.entity.lecture.LectureOption;
 import io.hhplus.tdd.lecture.infrastructure.db.entity.member.Member;
 import io.hhplus.tdd.lecture.infrastructure.db.repository.lecture.LectureApplyHistoryJpaRepository;
@@ -13,10 +15,10 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -85,6 +87,47 @@ public class LectureFacadeConcurrencyTest {
         assertThat(findLectureOption.getCurrentApplyCount())
             .isEqualTo(findLectureOption.getMaxApplyCount());
     }
+
+    @DisplayName("동일한 유저정보로 같은 특강을 5번 신청했을 때, 1번만 성공한다.")
+    @Test
+    void should_AllowOnly1Apply_When_SameUserApplies5Times() throws InterruptedException {
+        // given
+        int attemptCount = 5;
+        Lecture lecture = this.saveDefaultLecture();
+        LectureOption lectureOption = this.saveDefaultLectureOption(lecture.getLectureId());
+        Member member = this.saveMembers(1).get(0);
+
+        // when
+        AtomicInteger successCount = new AtomicInteger();
+        AtomicInteger failCount = new AtomicInteger();
+        ExecutorService executorService = Executors.newFixedThreadPool(attemptCount);
+        CountDownLatch latch = new CountDownLatch(attemptCount);
+
+        for (int i = 0; i < attemptCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    lectureFacade.applyLecture(member.getId(), lectureOption.getLectureOptionId());
+                    successCount.getAndIncrement();
+                } catch (LectureException e) {
+                    failCount.getAndIncrement();
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        executorService.shutdown();
+
+        //then
+        List<LectureApplyHistory> lectureApplyHistories =
+            lectureApplyHistoryJpaRepository.findAllByMemberIdAndSuccessIsTrue(member.getId());
+
+        assertThat(lectureApplyHistories).hasSize(1);
+        assertThat(successCount.get()).isEqualTo(1);
+        assertThat(failCount.get()).isEqualTo(attemptCount - 1);
+    }
+
 
     private Lecture saveDefaultLecture() {
         return lectureJpaRepository.save(
